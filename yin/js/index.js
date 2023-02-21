dec28bdc3604fb9104f087b6566cffd1293aa35d
m25563017 <71679886+m25563017@users.noreply.github.com>
2023-02-21 16:38:55 +0800

    照片更新

--- yin/js/index.js
+++ yin/js/index.js
@@ -10,7 +10,7 @@ function load(){
         clearInterval(timer);
         timer=setInterval((env) => {
             left = left - 2;
-            console.log(list.clientLeft);
+            // console.log(list.clientLeft);
             // 六張圖*寬度+magin
             // 還要加上6個margin
             if(left ==- (6 * 300 + 6 * 10)){ 
@@ -66,24 +66,14 @@ responsive: [
 });
 });
 
-// 滾動切換div
-// let div1 = document.getElementById('About_us');
-//         let div2 = document.getElementById('About_food');
-//         let div3 = document.getElementById('About_good');
-//         let activeDiv = div1;
-
-//         function scrollHandler(e) {
-//             if (e.deltaY > 0) {
-//                 activeDiv = activeDiv === div1 ? div2 : activeDiv === div2 ? div3 : div1;
-//             } else {
-//                 activeDiv = activeDiv === div1 ? div3 : activeDiv === div3 ? div2 : div1;
-//             }
-//             activeDiv.classList.add('ani');
-//             [div1, div2, div3].forEach((div) => {
-//                 if (div !== activeDiv) {
-//                     div.classList.remove('ani');
-//                 }
-//             });
-//         }
-
-//         window.addEventListener('wheel', scrollHandler);
+// about區塊
+    // var swiper = new Swiper(".mySwiper", {
+    //   direction: "vertical",
+    //   slidesPerView: 1,
+    //   spaceBetween: 30,
+    //   mousewheel: true,
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    //   },
+    // });
