31994e449bb47ffe62316c6f1678b9be2ccd747b
m25563017 <71679886+m25563017@users.noreply.github.com>
2023-03-01 09:57:05 +0800

    購物車輪播圖更新與視差改100vw

--- yin/js/Shoppingcar.js
+++ yin/js/Shoppingcar.js
@@ -14,3 +14,35 @@ document.addEventListener('scroll',function(){
   lastPos = currentPos;//再記住現在位置，跟未來的位置做比較
 });
 
+
+// 主要用於在網頁中跳轉到指定的錨點位置
+function scrollToAnchor(aid) {
+  // 使用 $("a[name='" + aid + "']") 語句查找錨點標籤
+  var aTag = $("a[name='" + aid + "']");
+  // aTag.offset().top 屬性來獲取標籤的實際位置，並將它作為 scrollTop 的值。
+  $('html,body').animate({ scrollTop: aTag.offset().top }, 'slow');
+}
+
+$("#home").click(function () {
+  scrollToAnchor('home');
+});
+
+$("#AboutHouse").click(function () {
+  scrollToAnchor('AboutHouse');
+});
+
+$("#AboutOwner").click(function () {
+  scrollToAnchor('AboutOwner');
+});
+
+$("#Photo").click(function () {
+  scrollToAnchor('Photo');
+});
+
+$("#Information").click(function () {
+  scrollToAnchor('Information');
+});
+
+$("#from").click(function () {
+  scrollToAnchor('from');
+});
