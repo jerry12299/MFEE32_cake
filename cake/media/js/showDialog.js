aaa99a67a069bee3bdbc09fb1341528bdd679244
ginnychen19 <71680206+ginnychen19@users.noreply.github.com>
2023-03-06 05:12:51 +0800

    0304~0306進度上傳
    01.詳細蛋糕切換圖片、輪播功能
    02.後臺訂單狀態視窗全更新~
    03.狀態浮窗顯示
    04.客製蛋糕介紹新增
    05.背景色修正

--- cake/media/js/showDialog.js
+++ cake/media/js/showDialog.js
@@ -1,6 +1,6 @@
 /* 詳細蛋糕頁，點擊出現浮窗 */
 const BTN = $('#buy');
-const showDialog = $("dialog");
+const showDialog = $("dialog.orderSuccess");
 BTN.on("click", function () {
     isPaused = true;
     showDialog.css({ display: "flex", });
@@ -28,17 +28,17 @@ function stopBubble(e) {
 
 /* 後臺頁，註銷成功出現浮窗 */
 const BTN_cancel = $('.btn_red');
-const showDialog_BEnd = $('.SomethingSuccess')
+const showDialog_BEnd = $('.ShowMSG')
 BTN_cancel.on("click", function () {
     let img = showDialog_BEnd.children().children().eq(1).find("img");
     let h6 = showDialog_BEnd.children().children().eq(1).find("h6");
-    
-    showDialog_BEnd.css({ display: "flex",});
-    showDialog_BEnd.children().css({border:"10px solid var(--BTN_Red)",backgroundColor:"var(--YWhite)"});
-    showDialog_BEnd.children().children().eq(0).css({backgroundColor:"var(--BTN_Red)",color:"var(--YWhite)"});
-    showDialog_BEnd.children().children().eq(0).find("h6").css({color:"var(--YWhite)"});
-    h6.css({color:"var(--BTN_Red)"})
-   
+
+    showDialog_BEnd.css({ display: "flex", });
+    showDialog_BEnd.children().css({ border: "10px solid var(--BTN_Red)", backgroundColor: "var(--YWhite)" });
+    showDialog_BEnd.children().children().eq(0).css({ backgroundColor: "var(--BTN_Red)", color: "var(--YWhite)" });
+    showDialog_BEnd.children().children().eq(0).find("h6").css({ color: "var(--YWhite)" });
+    h6.css({ color: "var(--BTN_Red)" })
+
     img.attr("src", "../Source/IMG/Key-Vision/Rabbit_NO.png");
     h6.text("註銷成功")
 })
